4cf68a2ecd3928caeba3511d2ec6581d7aaf2c93
AlexandreChaussard <alexandre.chaussard@telecom-sudparis.eu>
2021-10-04 12:35:26 +0200

    Intégration CSS et visual

--- proj-agvoy/agvoy-app/src/Controller/RoomController.php
+++ proj-agvoy/agvoy-app/src/Controller/RoomController.php
@@ -33,8 +33,23 @@ class RoomController extends AbstractController
      */
     public function showRoom(Room $room): Response
     {
+        $filteredRooms = array();
+        foreach($room->getRegions() as $region)
+        {
+
+            $filteredRoomsInRegion = $this->getRegionRooms($region, 6, $room);
+            foreach($filteredRoomsInRegion as $relatedRoom)
+            {
+                if(count($filteredRooms) >= 6)
+                    break;
+
+                array_push($filteredRooms, $relatedRoom);
+            }
+        }
+
         return $this->render('room/room_ad.html.twig', [
             'room' => $room,
+            'relatedRooms' => $filteredRooms,
         ]);
     }
 
@@ -43,28 +58,52 @@ class RoomController extends AbstractController
      */
     public function filteredRegion(Region $region): Response
     {
-        $em = $this->getDoctrine()->getManager();
-        // TODO: Demander si on peut pas faire que en SQL
-        //$rooms = $em->getRepository(Room::class)->findBy(['regions' => $region]);
-
-        $rooms = $em->getRepository(Room::class)->findAll();
-        $filteredRooms = array();
-        foreach($rooms as $room)
-        {
-            $regions = $room->getRegions();
-            foreach($regions as $reg)
-            {
-                if($region->getId() == $reg->getId())
-                {
-                    array_push($filteredRooms, $room);
-                    break;
-                }
-            }
-        }
+        $filteredRooms = $this->getRegionRooms($region, -1, null);
 
         return $this->render('room/filter.html.twig', [
             'rooms' => $filteredRooms,
             'region' => $region,
         ]);
     }
+
+    /**
+     * Récupère une liste de taille 'amount' de couettes et cafés dans la région donné hors 'except'
+     * Si amount < 0 alors on récupère toutes les chambres de la région
+     * @param Region $from
+     * @param int $amount
+     * @return array
+     */
+    private function getRegionRooms(Region $from, int $amount, Room $except)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $rooms = $em->getRepository(Room::class)->findAll();
+        $filteredRooms = array();
+
+        $i = 0;
+
+        foreach($rooms as $room)
+        {
+            if($amount > 0 &&
+                $i >= $amount)
+                break;
+            if($except != null &&
+                $room->getId() == $except->getId())
+                continue;
+
+            $regions = $room->getRegions();
+            foreach($regions as $reg)
+            {
+                if($from->getId() == $reg->getId())
+                {
+                    array_push($filteredRooms, $room);
+                    $i = $i + 1;
+                    break;
+                }
+            }
+        }
+
+        return $filteredRooms;
+    }
+
 }
